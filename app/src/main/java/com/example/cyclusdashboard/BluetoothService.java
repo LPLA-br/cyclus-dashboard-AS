063848610418de927a26eea53c1e97afed93feba
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2024-09-04 23:32:02 -0300

    feat: BluetoothService em progresso
    MainActivity com demonstracao de input e output

--- app/src/main/java/com/example/cyclusdashboard/BluetoothService.java
+++ app/src/main/java/com/example/cyclusdashboard/BluetoothService.java
@@ -1,36 +1,121 @@
 package com.example.cyclusdashboard;
 
+//IMPORTS COMPONENTES ANDROID
+import android.Manifest;
 import android.app.Service;
 import android.app.Activity;
-import android.bluetooth.le.BluetoothLeScanner;
-import android.content.Intent;
-import android.os.IBinder;
 
+import androidx.core.app.ActivityCompat; //permissões
+import androidx.core.content.PermissionChecker;
+
+import android.bluetooth.le.ScanResult;
+import android.content.Intent;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.os.Handler; //threads
+
+// IMPORTS BLUETOOTH
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+
+import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanSettings;
+
+import java.util.Vector;
 
 public class BluetoothService extends Service
 {
     protected BluetoothManager bluetoothManager;
     protected BluetoothAdapter bluetoothAdapter;
-    protected Activity atividade;
     protected BluetoothLeScanner bluetoothLeScanner;
+    protected BluetoothDevice bluetoothDevice;
 
-    public BluetoothService( Activity atividade )
+    protected final String targetEsp32ServiceUUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+    protected final String targetEsp32CharacteristicUUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+
+    boolean scanning;
+    int SCAN_TIME = 10000;
+    private final Vector<String> escaneados;
+
+    public BluetoothService()
+    {
+        this.scanning = false;
+        this.escaneados = new Vector<String>(255);
+    }
+
+    //SETOR DE CALLBACKS PARA: ScanCallback() usado por this.bluetoothLeScanner
+
+    private ScanCallback leScanCallback  = new ScanCallback()
+    {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result)
+        {
+            super.onScanResult(callbackType, result);
+            // getDevice() retorna BluetoothDevice
+            // escaneados não alterável d'aqui pois aqui é a famigerada callback.
+            this.escaneados.add(result.getDevice());
+        }
+    };
+
+    //SETOR DE MÉTODOS DE FUNÇÕES
+
+    private void scanLeDevice()
+    {
+        if (!scanning)
+        {
+            //thread será executada em 10 segundos
+            handler.postDelayed( new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    scanning = false;
+
+                    if (checkSelfPermission( this, Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED)
+                    {
+                        bluetoothLeScanner.stopScan(new ScanCallback()
+                        {
+                            @Override
+                            public void onScanResult(int callbackType, ScanResult result)
+                            {
+                                super.onScanResult(callbackType, result);
+                            }
+                        });
+                    }
+                    else stopSelf();
+                }
+            }, SCAN_PERIOD);
+            scanning = true;
+            bluetoothLeScanner.startScan(leScanCallback);
+        }
+        else
+        {
+            scanning = false;
+            bluetoothLeScanner.stopScan(leScanCallback);
+        }
+    }
+
+    //SETOR DE MÉTODOS SOBRESCRITOS DO SERVICE
+
+    @Override
+    public void onCreate()
     {
         this.bluetoothManager = getSystemService(BluetoothManager.class);
         this.bluetoothAdapter = bluetoothManager.getAdapter();
-        this.atividade = atividade;
+        this.bluetoothDevice = null;
         if (bluetoothAdapter == null)
         {
             // Device doesn't support Bluetooth
+            stopSelf();
         }
         else
         {
@@ -39,18 +124,19 @@ public class BluetoothService extends Service
                 //bluetooth inativo. Requisitar ativação ao usuário
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 //this.atividade.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                if(!bluetoothAdapter.isEnabled()) stopSelf();
             }
             else
             {
-                //obtem-se escaneador bluetooth low energy. Metodos agora
+                /*Procurar dispositivo para poder obter dispositivo via:
+                    this.bluetoothDevice().getRemoteDevice("MAC_BLE");
+                  Através de:
+                    this.bluetoothAdapter;
+                * */
                 this.bluetoothLeScanner = this.bluetoothAdapter.getBluetoothLeScanner();
+                //CONTINUA
             }
         }
-    }
-
-    public void buscarDispositivoBluetoothLE()
-    {
-        BluetoothLeScanner bluetoothLeScanner1 = this.bluetoothLeScanner;
     }
 
     @Override
--- app/src/main/java/com/example/cyclusdashboard/MainActivity.java
+++ app/src/main/java/com/example/cyclusdashboard/MainActivity.java
@@ -3,17 +3,25 @@ package com.example.cyclusdashboard;
 import android.annotation.SuppressLint;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+//SUB-COMPONENTES DA ACTIVITY PRINCIPAL (ELEMENTOS GRÁFICOS)
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity
 {
+
+    private EditText nomeDispServidorBluetooth;
+    private TextView textViewMain;
+    private Button conectarDispBluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,6 +34,20 @@ public class MainActivity extends AppCompatActivity
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //NOTA: (Foo) sample(); cast de tipo
+        nomeDispServidorBluetooth = (EditText) findViewById(R.id.nomeDispServidorBluetooth);
+        textViewMain = (TextView) findViewById(R.id.textViewMain);
+        conectarDispBluetooth = (Button) findViewById(R.id.conectarDispBluetooth);
+
+        conectarDispBluetooth.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                textViewMain.setText( (CharSequence) nomeDispServidorBluetooth.getText() );
+            }
         });
 
         //BluetoothGatt cliente = new BluetoothGatt();
